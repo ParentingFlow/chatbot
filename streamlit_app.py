9d2a3ca79c09524029df861c89ca784291950c6e
ParentingFlow <support@parentingflow.ai>
2025-08-16 21:45:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write(
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
+if openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
 
